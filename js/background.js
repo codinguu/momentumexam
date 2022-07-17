bddf07e254b73f5d6878a9d2b111e599b3cd2edb
dahaesol1 <dahaesol1@gmail.com>
2022-07-17 18:29:03 +0900

    css 완성

---
+++ js/background.js
@@ -0,0 +1,12 @@
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+bgImage = `img/${chosenImage}`;
+
+document.body.style.backgroundImage = `linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(${bgImage})`;
+/* const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage); */
